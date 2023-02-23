9ccde4b058b6a4de0b299e0f338dc0737eff9360
Thilan Wijesinghe <thilan.ruwanga665@gmail.com>
2023-02-24 00:01:56 +0530

    ab

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,7 +11,37 @@
     <link rel="icon" href="image/favicon-32x32.png">
 
 </head>
-<body>
-    
+
+<body class="bac">
+
+    <div class="container-fluid cen">
+        <div class="col-6  bg-white ">
+
+            <h3 class="offset-1 mt-4" style="color: #2bb3b1;">Join our community</h3> <br>
+            <h5 class="offset-1" style="color: #cadd6a;">30-day, hassle-free money back guarantee</h5>
+            <p class="offset-1" style="color: #bdbfc3;">Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are serious about honing their skills.</p>
+
+        </div>
+
+        <div class="col-6  d-block d-md-flex ">
+            <div class="row bg-black col-3">
+                <h5>fghgfds</h5>
+            </div>
+
+
+            <div class=" row bg-dark col-3  ">
+
+                <h5>sdfghgfds</h5>
+
+
+            </div>
+
+        </div>
+
+
+
+    </div>
+
 </body>
+
 </html>
