01417523e99f68e92165b1f4034b85ef3bd1f6ef
Thilan Wijesinghe <thilan.ruwanga665@gmail.com>
2023-02-24 13:48:38 +0530

    done

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 <body class="bac">
 
     <div class="container-fluid cen">
-        <div class="col-6  bg-white ">
+        <div class="col-12 col-lg-6  bg-white card shadow  bg-body bor " style="border-radius: 10px;">
             <div class="offset-1">
 
 
@@ -23,8 +23,8 @@
                 <h5 class="" style="color: #cadd6a;">30-day, hassle-free money back guarantee</h5>
                 <p class="" style="color: #bdbfc3;">Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are serious about honing their skills.</p>
             </div>
-            <div class="col-6  d-block d-md-flex ">
-                <div style="background-color: #2bb3b1;" class=" col-12">
+            <div class="col-12 col-lg-6 d-block d-md-flex ">
+                <div style="background-color: #2bb3b1;" class=" col-12 col-lg-12">
                     <div class="offset-1 mt-5">
                         <h5 style="color: white;" class="mb-3">Monthly Subscription</h5>
                         <h1 style="color: white;">$29</h1>
@@ -38,9 +38,9 @@
 
 
                 <div style="background-color: #4abebd;" class=" col-12  ">
-                    <div class="offset-1 mt-5">
+                    <div class="offset-1 mt-lg-5">
                         <h5 style="color: white;" class="mb-3">Why Us </h5>
-                        <p style="color: white; ">Tutorials by industry experts Peer & expert code review Coding exercises Access to our GitHub repos Community forum Flashcard decks New videos every week</p>
+                        <p style="color: white; ">Tutorials by industry experts <br>  Peer & expert code review <br> Coding exercises<br>  Access to our GitHub repos <br>  Community forum <br>  Flashcard decks <br>  New videos every week</p>
                     </div>
 
                 </div>
